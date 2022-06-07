a5a3cdf7f1c2457acd3e123bdb1f71370952b620
CameronHyden <camh.hyden@gmail.com>
2022-06-07 17:59:38 +0100

    created spring application

---
+++ src/main/java/com/example/fxjavaProject/FxJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fxjavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxJavaProjectApplication.class, args);
+	}
+
+}
